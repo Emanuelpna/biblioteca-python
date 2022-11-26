f27359c7bd99aa2daed2531796c7b54acb091a88
Emanuel Andrade <emanuelpna@yahoo.com.br>
2022-11-25 22:40:29 -0300

    updated BookController

--- controllers/BookController.py
+++ controllers/BookController.py
@@ -1,24 +1,29 @@
 from controllers.DatabaseController import DatabaseController
 from models.Book import Book
 
+
 class BookController:
 
     def __init__(self):
         self.__databaseController = DatabaseController(filePath='./data/livros.txt')
 
     def createBook(self, book):
+        codeBook = book.getCod()
         name = book.getName()
+        author = book.getAuthor()
         publisher = book.getPublisher()
         finePrice = book.getFinePrice()
-        author = book.getAuthor()
 
-        self.__databaseController.saveEntry(f'{name},{author},{publisher},{finePrice}')
+        self.__databaseController.saveEntry(f'{codeBook},{name},{author},{publisher},{finePrice}')
 
     def listBooks(self):
         books = self.__databaseController.getEntries()
         mappedBooks = []
         for book in books:
-            mappedBooks.append(Book(codBook=book[0], name=book[1], author=book[2], publisher=book[3], finePrice=book[4]))
+            if len(book) > 0:
+                mappedBooks.append(
+                    Book(codBook=book[0], name=book[1], author=book[2], publisher=book[3], finePrice=book[4])
+                )
         return mappedBooks
 
     def getBookByName(self, name):
@@ -26,4 +31,3 @@ class BookController:
         for book in books:
             if name == book.getName():
                 return book
-
