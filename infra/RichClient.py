0a3f225031caf0d58e97ec721f5f1aa555c57349
Emanuel Andrade <emanuelpna@yahoo.com.br>
2022-11-26 23:54:04 -0300

    updated printError missing on RichClient

--- infra/RichClient.py
+++ infra/RichClient.py
@@ -1,12 +1,10 @@
-from __future__ import annotations
-
 from pick import pick
 from rich import print as pretty_print
 from rich.prompt import Prompt
 from rich.table import Table
 from rich.panel import Panel
-from rich.columns import Columns
 from rich.tree import Tree
+from rich.traceback import install as tracebackInstall
 
 from typing import TYPE_CHECKING
 
@@ -15,6 +13,9 @@ if TYPE_CHECKING:
 
 
 class RichClient:
+    def __init__(self):
+        tracebackInstall()
+
     @staticmethod
     def inputData(message=''):
         return Prompt.ask(message)
@@ -32,7 +33,8 @@ class RichClient:
         tree = Tree(title)
 
         for option in options:
-            tree.add(f'[italic yellow]({option.get("id")})[/] {option.get("label")}')
+            tree.add(
+                f'[italic yellow]({option.get("id")})[/] {option.get("label")}')
 
         pretty_print(tree)
 
@@ -45,11 +47,7 @@ class RichClient:
 
     @staticmethod
     def printError(error):
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print('')
-        print(error)
-        print('')
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        pretty_print(Panel.fit(error, title="Erro!"))
 
     @staticmethod
     def printTable(tableOptions: 'TableOptions'):
