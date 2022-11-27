67c6518290519e6b1937cb554f30559b240ab001
Emanuel Andrade <emanuelpna@yahoo.com.br>
2022-11-26 23:54:26 -0300

    removing future import

--- views/BookView.py
+++ views/BookView.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import uuid
 
 from infra.TableOptions import TableOptions
@@ -20,8 +18,10 @@ class BookView:
     def printCreateBook(self):
         bookName = self.__printGenerator.inputData("Digite o Nome do livro")
         bookAuthor = self.__printGenerator.inputData("Digite o Autor do livro")
-        bookPublisher = self.__printGenerator.inputData("Digite a Editora do livro")
-        bookPrice = float(self.__printGenerator.inputData("Digite o preço do multa do empréstimo do livro"))
+        bookPublisher = self.__printGenerator.inputData(
+            "Digite a Editora do livro")
+        bookPrice = float(self.__printGenerator.inputData(
+            "Digite o preço do multa do empréstimo do livro"))
         codBook = f'Book-{uuid.uuid4()}'
 
         book = Book(codBook, bookName, bookPrice, bookAuthor, bookPublisher)
@@ -30,5 +30,6 @@ class BookView:
 
     def printBooks(self):
         books = self.__bookController.listBooks()
-        booksTable = TableOptions("Livros que possuimos", ["Código", "Nome", "Autor", "Editora", "Preço"], books)
+        booksTable = TableOptions("Livros que possuimos", [
+                                  "Código", "Nome", "Autor", "Editora", "Preço"], books)
         self.__printGenerator.printTable(booksTable)
